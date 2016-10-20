016cb3c3ca01e92d2b815ca981869f591bedc1e8
Ankur Verma <anksv@users.noreply.github.com>
2016-10-21 01:31:14 +0530

    Create weighted.go

---
+++ weighted.go
@@ -0,0 +1,20 @@
+package main
+
+import(
+"fmt"
+)
+
+func link_netns( finalmap map[string]string){
+
+}
+
+func get_veth( cname string){
+
+
+
+}
+
+func allot_network_share( finalmap map[string]string){
+
+}
+
